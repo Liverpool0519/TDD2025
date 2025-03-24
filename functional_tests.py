9a70b6a1afe814e55a129a6304ba553cd7d446c0
nqq <niqingquan2004@163.com>
2025-03-24 11:32:13 +0800

    message 5: The first iterative development

--- functional_tests.py
+++ functional_tests.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
         # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
         # 他注意到网页里包含“To-Do”这个词
-        self.assertEqual('To-Do', self.browser.title, "browser title was:" + self.browser.title)
+        self.assertIn('To-Do', self.browser.title, "browser title was:" + self.browser.title)
         self.fail('Finish the test!')
         # 应用有一个输入待办事项的文本输入框
         # 他在文本输入框中输入了"Buy flowers"
